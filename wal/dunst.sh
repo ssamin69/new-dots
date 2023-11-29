c800806eeb07cf6015fe34e99b2962256f57050d
same thing <72408262+ssamin69@users.noreply.github.com>
2023-11-29 10:07:58 +0600

    Add files via upload

--- wal/dunst.sh
+++ wal/dunst.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-killall -SIGUSR2 waybar
+
 pywalfox update
+killall -SIGUSR2 waybar
+
 
 mkdir -p  "${HOME}/.config/dunst"
 ln -sf    "${HOME}/.cache/wal/dunstrc"    "${HOME}/.config/dunst/dunstrc"
