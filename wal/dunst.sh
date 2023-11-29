f4491128400b0a7cc8a75d4d6d23c512ff7449f0
same thing <72408262+ssamin69@users.noreply.github.com>
2023-11-29 09:28:38 +0600

    Add files via upload

--- wal/dunst.sh
+++ wal/dunst.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 killall -SIGUSR2 waybar
-
-
 pywalfox update
 
 mkdir -p  "${HOME}/.config/dunst"
 ln -sf    "${HOME}/.cache/wal/dunstrc"    "${HOME}/.config/dunst/dunstrc"
 
+
+
 pkill dunst 
 dunst &
+
+
+
+
 
 
 
